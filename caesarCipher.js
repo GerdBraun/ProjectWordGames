b8f4f72ddf81d7d6d8e0981cc3d5e8f20131eb52
Gerd Braun <webmaster@your-d-sign.de>
2024-10-04 10:05:32 +0200

    added decoding method

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,17 +1,22 @@
 // Get input from the command line
 const args = process.argv.slice(2);
 // Check if the user provided exactly two arguments
-if (args.length !== 2) {
-    console.error('I need exactly one string & 1 number, in this exact order!');
+if (args.length !== 3) {
+    console.error('I need exactly 2 strings & 1 number, in this exact order! ("method" "string to encode" shift)');
     return;
 }
 // Check if the first argument is a string
 if (typeof args[0] !== 'string') {
-    console.error('First argument needs to be a string!');
+    console.error('First argument needs to be a string! ("encode" or "decode")');
+    return;
+}
+// Check if the second argument is a string
+if (typeof args[1] !== 'string') {
+    console.error('Second argument needs to be a string!');
     return;
 }
 // Check if the second argument is a number
-if ( isNaN(args[1])) {
+if (isNaN(args[2])) {
     console.error('Second argument needs to be a number!');
     return;
 }
@@ -23,10 +28,27 @@ const alphabetLength = alphabetString.length;
 // for the translated words to be  stored
 let outputArray = [];
 
+// the action to take (encode or decode)
+const action = args[0];
 // from user input (latin has no upper or lower case)
-const inputString = args[0].toLowerCase();
+const inputString = args[1].toLowerCase();
+
+let shiftToUse = args[2];
+switch (action) {
+    case 'encode':
+        // do nothing - take shift as it is
+        break;
+    case 'decode':
+        // reverse shift
+        shiftToUse *= -1;
+        break;
+    default:
+        // create error message
+        console.error(action,' is neither "encode" nor "decode');
+}
+
 // get shift from user input  and make sure it's value is in bounds
-const shift = parseInt(args[1]) % alphabetLength;
+const shift = parseInt(shiftToUse) % alphabetLength;
 
 // translate single words
 const translateWord = (word) => {
